de9cac8984a53560b452c665159f2e2309d3721b
Madeline Pickering <madelinepickering@gmail.com>
2016-07-04 13:38:25 -0600

    longer description field in db

--- migrations/20160509141055_create_tables.js
+++ migrations/20160509141055_create_tables.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('podcasts', function(table){
   table.increments('id');
   table.string('title');
-  table.string('description');
+  table.string('description', 1000);
   table.date('date');
   table.string('link');
   table.string('image_url');
